97df7949a52111c547e773a3693ff0b010f50c55
Silentor <Silentor@users.noreply.github.com>
2017-02-21 23:25:10 +0200

    Add Vector2 based get random range extension

---
+++ Assets/Scripts/Tools/RandomExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace TerrainDemo.Tools
+{
+    public static class RandomExtensions
+    {
+        public static int GetRandomRange(this Vector2 value)
+        {
+            return Mathf.RoundToInt(Random.Range(value.x, value.y));
+        }
+    }
+}
---
+++ Assets/Tests/Editor/LandLayoutTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using TerrainDemo.Layout;
+using TerrainDemo.Libs.SimpleJSON;
+using TerrainDemo.Settings;
+using TerrainDemo.Voronoi;
+using UnityEditor;
+using UnityEngine;
+
+namespace TerrainDemo.Tests.Editor
+{
+    public class LandLayoutTests
+    {
+        [Test]
+        public void TestGetChunks()
+        {
+            var data = (TextAsset)EditorGUIUtility.Load("Tests/Cellmesh0.json");
+            var mesh = CellMesh.FromJSON(JSONNode.Parse(data.text));
+            var settings = Object.FindObjectOfType<LandSettings>();
+            //var clusters = new[]
+            //{
+            //    new ClusterInfo() {
+            //        Id = 0, ZoneHeights = new[] { Vector3.zero, Vector3.one*10 },
+            //        ClusterHeights = new[] { Vector3.zero, Vector3.one*10 },
+            //        Type = settings.Zones[0].Type,
+            //        Neighbors = new ClusterInfo[0],
+            //        Cells = new CellMesh.Submesh(mesh, )
+
+
+
+
+
+            //    }, 
+            //}
+
+            //var landLayout = new LandLayout(settings, mesh, );
+
+
+            var finder = mesh.FloodFill(mesh[0]);
+            var neighbors2 = finder.GetNeighbors(2);
+
+            Assert.That(mesh[0].Neighbors2, Is.EquivalentTo(neighbors2));
+        }
+      
+    }
+}
