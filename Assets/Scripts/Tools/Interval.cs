cfb40fe674acba5da7f84ee07c60c964244af8bc
Silentor <Silentor666@gmail.com>
2019-02-12 00:43:11 +0200

    added simple Interval data type for handy Block layers visualization

---
+++ Assets/Scripts/Tools/Interval.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+namespace TerrainDemo.Tools
+{
+    /// <summary>
+    /// Inclusive range between min and max float values
+    /// </summary>
+    public readonly struct Interval : IEquatable<Interval>
+    {
+        public readonly float Min;
+        public readonly float Max;
+        public bool IsEmpty => Min == 0 && Max == 0;
+
+        public static readonly Interval Empty = new Interval(0, 0);
+
+        public Interval(float min, float max)
+        {
+            if (max < min)
+            {
+                max = min = 0;
+            }
+
+            Min = min;
+            Max = max;
+        }
+
+        public bool Contains(float value)
+        {
+            return value >= Min && value <= Max;
+        }
+
+        public bool IsIntersect(Interval other)
+        {
+            return !(other.Min > Max || other.Max < Min);
+        }
+
+        public IEnumerable<Interval> Subtract(Interval other)
+        {
+            if (IsEmpty)
+            {
+                yield return Empty;
+                yield break;
+            }
+
+            if (other.IsEmpty || !IsIntersect(other))
+            {
+                yield return this;
+                yield break;
+            }
+
+            if (other.Min <= Min)
+            {
+                if (other.Max < Max)
+                {
+                    yield return new Interval(other.Max, Max);
+                }
+            }
+            else
+            {
+                yield return new Interval(Min, other.Min);
+
+                if (other.Max < Max)
+                {
+                    yield return new Interval(other.Max, Max);
+                }
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"({Min}-{Max}";
+        }
+
+        #region IEquatable
+
+        public bool Equals(Interval other)
+        {
+            return Min.Equals(other.Min) && Max.Equals(other.Max);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Interval other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Min.GetHashCode();
+                hashCode = (hashCode * 397) ^ Max.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Interval left, Interval right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Interval left, Interval right)
+        {
+            return !left.Equals(right);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Tests/Editor/IntervalTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using TerrainDemo.Tools;
+using UnityEngine.TestTools;
+
+namespace TerrainDemo.Tests.Editor
+{
+    public class IntervalTests
+    {
+
+        [Test]
+        public void TestContains()
+        {
+            var interval = new Interval(0, 5);
+
+            Assert.That(interval.Contains(3), Is.True);
+            Assert.That(interval.Contains(0), Is.True);
+            Assert.That(interval.Contains(5), Is.True);
+            Assert.That(interval.Contains(-1), Is.False);
+        }
+
+        [Test]
+        public void TestConstructor()
+        {
+            var interval = new Interval(-1, 3);
+
+            Assert.That(interval.IsEmpty, Is.False);
+            Assert.That(interval, Is.Not.EqualTo(Interval.Empty));
+            Assert.That(interval != Interval.Empty, Is.True);
+
+            interval = new Interval(3, -1);
+
+            Assert.That(interval.IsEmpty, Is.True);
+            Assert.That(interval, Is.EqualTo(Interval.Empty));
+            Assert.That(interval == Interval.Empty, Is.True);
+        }
+
+        [Test]
+        public void TestIntersect()
+        {
+            var interval1 = new Interval(0, 5);
+            var interval2 = new Interval(-5, -1);
+            var interval3 = new Interval(5, 10);
+            var interval4 = new Interval(-1, 15);
+            var interval5 = new Interval(0, 2);
+            var interval6 = new Interval(3, 5);
+            var interval7 = new Interval(2, 3);
+
+            Assert.That(interval1.Subtract(interval2), Is.EquivalentTo(new []{interval1}));
+            Assert.That(interval1.Subtract(interval3), Is.EquivalentTo(new[] { interval1 }));
+            Assert.That(interval1.Subtract(interval4), Is.Empty);
+            Assert.That(interval1.Subtract(interval5), Is.EquivalentTo(new []{new Interval(2, 5)}));
+            Assert.That(interval1.Subtract(interval6), Is.EquivalentTo(new[] { new Interval(0, 3) }));
+            Assert.That(interval1.Subtract(interval7), Is.EquivalentTo(new[]{new Interval(0, 2), new Interval(3, 5)}));
+        }
+
+    }
+}
